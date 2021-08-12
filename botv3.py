fdae69a079b1e2ce9105c3b83884b778006ad89d
Vladislav Priske <55079623+wemitwqw@users.noreply.github.com>
2021-08-12 18:37:31 +0300

    Update botv3.py

--- botv3.py
+++ botv3.py
@@ -3,25 +3,24 @@ import pandas as pd
 import json
 
 req = requests.get('https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets=btc&'
-                   'metrics=CapRealUSD,CapMrktCurUSD&limit_per_asset=2').json()
+                   'metrics=CapRealUSD,CapMrktCurUSD&limit_per_asset=3').json()
 print(req)
 req = str(req['data'])
 req = req.replace("\'", "\"")
-req = req.replace("[", "")
-req = req.replace("]", "")
-print(req)
+#req = req.replace("[", "")
+#req = req.replace("]", "")
+#print(req)
 req = json.loads(req)
 #print(req['time'])
 #nupl = int(req['CapMrktCurUSD'])
+print(req[1]['time'])
+df = pd.DataFrame.from_dict(req)
 
-
-'''json.loads(req)
 pd.set_option('display.max_colwidth', None)
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-df = pd.DataFrame.from_dict(req)
-
 df.columns = ['Asset', 'Time', 'Realized Cap', 'Market Cap']
-print(df.to_string(index=False))'''
+df = (df[['Time', 'Realized Cap', 'Market Cap']])
+print(df.to_string(index=False))
